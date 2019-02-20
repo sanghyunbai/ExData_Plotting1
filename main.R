183db0f6669ad1d3e63e4afa9c9caebf4cb2c53f
sanghyun <sanghyun@kth.se>
2019-02-21 00:35:57 +0100

    Plot1 implemented, and created plot1.png

--- main.R
+++ main.R
@@ -22,10 +22,16 @@ readData <- function(){
         # print(head(data))
         data        
 }
+plot1 <- function(data){
+        png("plot1.png", width=480, height=480)
+        hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+        dev.off() 
+}
 
 run <- function(){
         library(dplyr)
         datasets <- readData()   
-        
+        ## plot1 
+        plot1(datasets)
 }
 run()
