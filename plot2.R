261c22fcd92aee100c21fa6b9b6c8654b336d3f4
sanghyun <sanghyun@kth.se>
2019-02-22 22:35:10 +0100

    plot2.R separated from main.R, also, plot2.png generated with empthy xlab

---
+++ plot2.R
@@ -0,0 +1,35 @@
+readData <- function(){
+        fileName <- "household_power_consumption.txt"
+        filePath = paste(getwd(),fileName,sep = "/")
+        # print(filePath)
+        data <- read.table(filePath,header = TRUE,sep = ";",
+                           stringsAsFactors = FALSE,
+                           # colClasses = c("Date","Time","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+        )
+        # print(colnames(data))
+        data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+        # print(head(data))
+        
+        data$Global_active_power <- as.numeric(data$Global_active_power)
+        data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+        data$Voltage <- as.numeric(data$Voltage)
+        data$Global_intensity <- as.numeric(data$Global_intensity)
+        data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+        data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+        data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+        # print(head(data))
+        data        
+}
+plot2 <-function(data){
+        timeAndDate <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+        png("plot2.png", width=480, height=480)
+        plot(timeAndDate, data$Global_active_power, type="l", xlab="",ylab="Global Active Power (kilowatts)")
+        dev.off()        
+}
+run <- function(){
+        library(dplyr)
+        datasets <- readData()   
+        # plot2
+        plot2(datasets)
+}
+run()
